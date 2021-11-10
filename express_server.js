e388054dbb7a9665dc898a53cc7b0d189adb1156
Mojdeh Sabz <mojdeh.sbzgh@gmail.com>
2021-11-10 09:34:40 -0500

    install cookie-parse

--- express_server.js
+++ express_server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -7,6 +8,7 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(cookieParser());
 
 
 const generateRandomString = function() {
