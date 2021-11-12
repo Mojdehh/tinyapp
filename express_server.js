f315f71720fd21d09866db35f214f4a957ff26b0
Mojdeh Sabz <mojdeh.sbzgh@gmail.com>
2021-11-11 23:12:33 -0500

    install bcrypt package

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const bcrypt = require('bcryptjs');
 const app = express();
 const PORT = 8080; // default port 8080
 
